9fe01d304e416dd352ac39e5879aa485b15cd50a
desmond333 <my_post_office1@mail.ru>
2020-10-24 17:58:03 +0300

    Updates

---
+++ precache-manifest.229fc60989889d91a90717e57fcc2694.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c240617827b5bf966183",
+    "url": "/cyber/static/css/main.31b0414e.chunk.css"
+  },
+  {
+    "revision": "c240617827b5bf966183",
+    "url": "/cyber/static/js/main.c2406178.chunk.js"
+  },
+  {
+    "revision": "885f1f58311e58c28c9d",
+    "url": "/cyber/static/js/1.885f1f58.chunk.js"
+  },
+  {
+    "revision": "65ce02e19b9a28045534",
+    "url": "/cyber/static/js/runtime~main.65ce02e1.js"
+  },
+  {
+    "revision": "b778458319d11d3ad7716809eab7c1b0",
+    "url": "/cyber/static/media/MyNetworkName.b7784583.svg"
+  },
+  {
+    "revision": "5d54df1e405ef40d84d4fa1d4eb4fb92",
+    "url": "/cyber/static/media/preloader.5d54df1e.svg"
+  },
+  {
+    "revision": "1f33a1e6e945b293493544d29d10e774",
+    "url": "/cyber/static/media/ProfileSlava.1f33a1e6.png"
+  },
+  {
+    "revision": "2ae3ca68ec215e744509a131f98e5a9b",
+    "url": "/cyber/index.html"
+  }
+];
